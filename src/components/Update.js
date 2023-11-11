cf81c233a39bf20675f86336454c226cbcaf130f
Authoi234 <jawadkhan.authoi@gmail.com>
2023-11-12 01:55:14 +0630

    this is fully ready

--- src/components/Update.js
+++ src/components/Update.js
@@ -7,7 +7,20 @@ const Update = () => {
 
     const handleUpdateUser = event => {
         event.preventDefault();
-        console.log(user);
+        // console.log(user);
+        fetch(`http://localhost:5000/users/${storedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.modifiedCount > 0) {
+                alert('User Updated');
+            }
+        })
     }
 
     const handleInputChange = event => {
